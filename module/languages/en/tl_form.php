edb63f17d05e92941d624602712b91abfcb0ce07
Thomas Schabacher <schabacher@dmkzwo.de>
2017-05-16 13:15:24 +0200

    english translation for tl_form added

---
+++ module/languages/en/tl_form.php
@@ -0,0 +1,19 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_form']['dz_autoreply_legend'] = 'Autoreply';
+
+$GLOBALS['TL_LANG']['tl_form']['dz_add_autoreply'][0] = 'use autoreply';
+$GLOBALS['TL_LANG']['tl_form']['dz_add_autoreply'][1] = '';
+
+$GLOBALS['TL_LANG']['tl_form']['dz_autoreply_email_field'][0] = 'Formfield used as mailto';
+$GLOBALS['TL_LANG']['tl_form']['dz_autoreply_email_field'][1] = '';
+
+$GLOBALS['TL_LANG']['tl_form']['dz_autoreply_mailtext'][0] = 'Mailtext';
+$GLOBALS['TL_LANG']['tl_form']['dz_autoreply_mailtext'][1] = 'use {{formfieldname}} to insert form data';
+
+$GLOBALS['TL_LANG']['tl_form']['dz_autoreply_mailfrom'][0] = 'Sender';
+$GLOBALS['TL_LANG']['tl_form']['dz_autoreply_mailfrom'][1] = '';
+
+$GLOBALS['TL_LANG']['tl_form']['dz_autoreply_mailsubject'][0] = 'Subject';
+$GLOBALS['TL_LANG']['tl_form']['dz_autoreply_mailsubject'][1] = 'use {{formfieldname}} to insert form data';
+
