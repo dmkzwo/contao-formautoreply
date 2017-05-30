e0bcf043835b2a10b723fce7f6fcdcd2d495e88b
Thomas Schabacher <schabacher@dmkzwo.de>
2017-05-30 16:51:27 +0200

    ---

---
+++ config/autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+if (class_exists('NamespaceClassLoader')) {
+  /**
+   * Register PSR-0 namespace
+   */
+  NamespaceClassLoader::add('Dmkzwo', 'system/modules/formautoreply/library');
+
+}
